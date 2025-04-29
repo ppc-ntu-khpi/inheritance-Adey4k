52b82bbb520ac7c440022f13c5e384651fa2d897
Ладіков Максим <150963491+Adey4k@users.noreply.github.com>
2025-04-29 17:32:10 +0300

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Ladik
+ */
+import domain.Elephant;
+
+/**
+ *
+ * @author Ladik
+ */
+        public static void main(String[] args){
+            Elephant elephant = new Elephant("Dane");
+            System.out.println(elephant);
+            elephant.move();
+            elephant.eat();
+            elephant.breathe();
+            elephant.draw();
+        } 
